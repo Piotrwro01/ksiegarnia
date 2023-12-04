7c283ce3c5794c9c4c75529a424ada18f76d62a9
PiotrWrotny <piotrwro01@gmail.com>
2023-12-04 21:08:25 +0100

    git git

---
+++ ksiegarnia/Ksiegarnia/Controller/KontrolerKsiazki.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksiegarnia.Controller
+{
+    internal class KontrolerKsiazki
+    {
+        // opcjonalny, można zrobić duży kontroler uposażenia
+        // logika na podmiane ceny
+        // podmiane tytulu
+        // podmianę autora itp
+    }
+}
---
+++ ksiegarnia/Ksiegarnia/Model/Konto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksiegarnia.Model
+{
+    internal class Konto
+    {
+        // konto jako stan konta księgarnii
+        // pola Konto
+        // konstruktor książki
+        // podstawowa logika 
+    }
+}
